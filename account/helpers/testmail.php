7cdf47167a68194c7783ccf549c04f2236d3a31a
Mark Verbunt <markverbunt@Paulios-MacBook-Pro.local>
2020-10-30 20:17:28 +0100

    added phpmailer

--- account/helpers/testmail.php
+++ account/helpers/testmail.php
@@ -1,48 +1,34 @@
 <?php
-// Multiple recipients
-$to = 'm_verbunt@hotmail.com, m_verbunt@hotmail.com'; // note the comma
 
-// Subject
-$subject = 'Birthday Reminders for August';
+/**
+ * This example shows sending a message using PHP's mail() function.
+ */
 
-// Message
-$message = '
-<html>
-<head>
-  <title>Birthday Reminders for August</title>
-</head>
-<body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
-</body>
-</html>
-';
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
 
-// To send HTML mail, the Content-type header must be set
-$headers[] = 'MIME-Version: 1.0';
-$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+require '../../vendor/autoload.php';
 
-// Additional headers
-$headers[] = 'To: Mark <m_verbunt@hotmail.com>, Mark <m_verbunt@hotmail.com>';
-$headers[] = 'From: Mark <info@svreeshof-bierteam.nl>';
-$headers[] = 'Cc: m_verbunt@hotmail.com';
-$headers[] = 'Bcc: m_verbunt@hotmail.com';
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+//Set who the message is to be sent from
+$mail->setFrom('m_verbunt@hotmail.com', 'Mark');
+//Set an alternative reply-to address
+$mail->addReplyTo('m_verbunt@hotmail.com', 'Mark');
+//Set who the message is to be sent to
+$mail->addAddress('m_verbunt@hotmail.com', 'Mark');
+//Set the subject line
+$mail->Subject = 'PHPMailer mail() test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+//$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+//Replace the plain text body with one created manually
+$mail->Body = 'This is a plain-text message body';
+$mail->AltBody = 'This is a plain-text message body';
 
-// Mail it
-mail($to, $subject, $message, implode("\r\n", $headers));
-if($mail){
-  echo "Thank you for using our mail form";
-}else{
-  echo "Mail sending failed."; 
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
 }
-?>
