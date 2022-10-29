8bff286ef0d23cf9b79c02e3f9b35a2f01991aed
Mark Verbunt <markverbunt@Paulios-MacBook-Pro.local>
2022-10-29 18:40:18 +0200

    change qr code and link

--- account/finance.php
+++ account/finance.php
@@ -90,7 +90,7 @@ require_once('helpers/global_vars.php');
                             <h2>Betaal direct</h2>
                         </div>
                         <div class="body">
-                            <a href="https://betaalverzoek.rabobank.nl/betaalverzoek/?id=iR33NJWFRpufUQs845ewsQ" target="_blank"><img class="img-responsive" src="/images/QR-code-2022.png" alt="QR code"></a>
+                            <a href="https://betaalverzoek.rabobank.nl/betaalverzoek/?id=rqSVITO8Rh2J3CvWQ8UWDQ" target="_blank"><img class="img-responsive" src="/images/QR-code-2022.png" alt="QR code"></a>
                             <p style="text-align:center;"><a href="https://betaalverzoek.rabobank.nl/betaalverzoek/?id=rqSVITO8Rh2J3CvWQ8UWDQ" target="_blank">Betaal hier direct</a></p>
                         </div>
                     </div>
